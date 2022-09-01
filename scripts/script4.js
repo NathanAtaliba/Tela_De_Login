dfd02b14c9e4c81e6d57889dabdf9c87e11e5f71
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-09-01 00:42:14 -0300

    Tela_De_Logim

---
+++ scripts/script4.js
@@ -0,0 +1,11 @@
+function limpar(){
+    var cor = window.document.getElementById('button')
+var nickname = window.document.getElementById('nickname')
+var email = window.document.getElementById('email')
+var password = window.document.getElementById('password')
+var emailR = window.document.getElementById('emailR')
+nickname.value = ``
+email.value = ``
+password.value = ``
+emailR.value = ``
+}
